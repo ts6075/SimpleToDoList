36795b725852b70bab81cd2e19a0bd881b983e3d
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-05 23:23:25 +0800

    feat: 畫面切版、加入內頁、移除ActionBar

--- app/src/main/java/com/goodtodo/simpletodolist/MainActivity.java
+++ app/src/main/java/com/goodtodo/simpletodolist/MainActivity.java
@@ -2,10 +2,16 @@ package com.goodtodo.simpletodolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void goToAddTaskView(View v) {
+        Intent intentMain = new Intent(MainActivity.this, AddTaskActivity.class);
+        startActivityForResult(intentMain, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                String title = data.getStringExtra("title");
+                String txt = data.getStringExtra("txt");
+                String date = data.getStringExtra("date");
+                String delete = data.getStringExtra("delete");
+                String category = data.getStringExtra("categorie");
+                SimpleDateFormat newDateFormat = new SimpleDateFormat("EE d MMM yyyy k:m");
+                Date d = null;
+                try {
+                    d = newDateFormat.parse(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                if (data.getStringExtra("edit").equals("true")) {
+                } else {
+                }
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //here goes nothing
+            }
+        }
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+            }
+        }
     }
 }
