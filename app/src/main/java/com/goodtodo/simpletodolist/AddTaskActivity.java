608b383b57720b861bfc58ed9494fe24950c94c6
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-11 13:50:15 +0800

    feat: 新增待辦事項(暫先以Toast取代)

--- app/src/main/java/com/goodtodo/simpletodolist/AddTaskActivity.java
+++ app/src/main/java/com/goodtodo/simpletodolist/AddTaskActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,13 +15,23 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
     }
 
+    /**
+     * 新增待辦事項
+     * @param v
+     */
     public void addTask(View v) {
-
+        String taskTitle = ((TextView)findViewById(R.id.taskTitle)).getText().toString();
+        String taskContent = ((TextView)findViewById(R.id.taskContent)).getText().toString();
+        Toast.makeText(this, "輸入的標題：" + taskTitle + "\n輸入的內容：" + taskContent, Toast.LENGTH_SHORT).show();
+        finish();
     }
 
+    /**
+     * 關閉新增待辦事項頁面
+     * @param v
+     */
     public void closeAddTaskView(View v) {
         Intent returnIntent = new Intent();
         setResult(Activity.RESULT_CANCELED, returnIntent);
